3700e5c09b6e30ed90a4f108a3ca3e867fee6f4e
Robert Sargeant Shaw III (Ross Shaw) <VicisVitium@gmail.com>
2020-02-03 19:21:29 -0800

    Set up NUnit test framework.

---
+++ CardEngine.Library.Tests/CardEngineTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardEngine.Library.Tests
+{
+    [TestFixture]
+    public class CardEngineTests
+    {
+        [Test]
+        public void TestFramework_Active()
+        {
+            Assert.Pass();
+        }
+    }
+}
