48588fe4bec45669d57406def0a231e63d260e8c
Robert Sargeant Shaw III (Ross Shaw) <VicisVitium@gmail.com>
2020-02-03 21:56:37 -0800

    Create card class and tests.

--- CardEngine.Library.Tests/CardEngineTests.cs
+++ CardEngine.Library.Tests/CardEngineTests.cs
@@ -15,5 +15,78 @@ namespace CardEngine.Library.Tests
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void CardEngine_CreateCard_ReturnsCard()
+        {
+            string val = "Ace";
+            string suit = "Clubs";
+            Card card = CardEngine.CreateCard(val, suit);
+            Assert.IsInstanceOf<Card>(card);
+            Assert.AreEqual(val, card.String_Value);
+            Assert.AreEqual(suit, card.Suit);
+        }
+
+        [Test]
+        public void CardEngine_ClassCardGet_ReturnsValueOrFace(
+            [Values("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King")] string val
+            )
+        {
+            string suit = "Clubs";
+            Card card = CardEngine.CreateCard(val, suit);
+            Assert.AreEqual(val, card.String_Value);
+        }
+
+        [TestCase("1", "Ace")]
+        [TestCase("10", "Jack")]
+        [TestCase("11", "Queen")]
+        [TestCase("12", "King")]
+        public void CardEngine_ClassCardConstructor_ConvertsToFace(string val, string expected)
+        {
+            string suit = "Clubs";
+            Card card = CardEngine.CreateCard(val, suit);
+            Assert.AreEqual(expected, card.String_Value);
+        }
+
+        [Test]
+        public void CardEngine_ClassCardConstructor_DoesNotAcceptInvalidValues(
+            [Values("-1", "14", "foo")] string val
+            )
+        {
+            string suit = "Clubs";
+            Assert.Throws<ArgumentException>(delegate { CardEngine.CreateCard(val, suit); } );
+        }
+
+        [Test]
+        public void CardEngine_ClassCardConstructor_DoesNotAcceptInvalidSuits(
+            [Values("-1", "14", "foo")] string suit
+            )
+        {
+            string val = "1";
+            Assert.Throws<ArgumentException>(delegate { CardEngine.CreateCard(val, suit); });
+        }
+
+        [TestCase("Ace", 1)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        [TestCase("4", 4)]
+        [TestCase("5", 5)]
+        [TestCase("6", 6)]
+        [TestCase("7", 7)]
+        [TestCase("8", 8)]
+        [TestCase("9", 9)]
+        [TestCase("Jack", 10)]
+        [TestCase("10", 10)]
+        [TestCase("Queen", 11)]
+        [TestCase("11", 11)]
+        [TestCase("King", 12)]
+        [TestCase("12", 12)]
+        public void CardEngine_ClassCardGetIntVal_ReturnsIntegerValueOfCard(string val, int expected)
+        {
+            string suit = "Clubs";
+            Card card = new Card(val, suit);
+            Assert.AreEqual(expected, card.Integer_Value);
+        }
     }
 }
--- CardEngine.Library/CardEngine.cs
+++ CardEngine.Library/CardEngine.cs
@@ -6,4 +6,93 @@ using System.Threading.Tasks;
 
 namespace CardEngine.Library
 {
+    public class Card
+    {
+        private string string_value;
+        private string suit;
+
+        public string String_Value
+        {
+            get => string_value;
+            set
+            {
+                if (value.ToLower() == "1" || value.ToLower() == "ace")
+                {
+                    string_value = "Ace";
+                    Integer_Value = 1;
+                }
+                else if (value.ToLower() == "10" || value.ToLower() == "jack")
+                {
+                    string_value = "Jack";
+                    Integer_Value = 10;
+                }
+                else if (value.ToLower() == "11" || value.ToLower() == "queen")
+                {
+                    string_value = "Queen";
+                    Integer_Value = 11;
+                }
+                else if (value.ToLower() == "12" || value.ToLower() == "king")
+                {
+                    string_value = "King";
+                    Integer_Value = 12;
+                }
+                else
+                {
+                    try
+                    {
+                        int checkVal = Convert.ToInt32(value);
+                        if (1 < checkVal && checkVal < 10)
+                        {
+                            string_value = value;
+                            Integer_Value = Convert.ToInt32(value);
+                        }
+                        else
+                        {
+                            throw new ArgumentException();
+                        }
+                    }
+                    catch
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+            }
+        }
+
+        public string Suit
+        {
+            get => suit;
+            set
+            {
+                if (value.ToLower() == "clubs" || 
+                    value.ToLower() == "diamonds" ||
+                    value.ToLower() == "hearts" ||
+                    value.ToLower() == "spades"
+                    )
+                {
+                    suit = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        public int Integer_Value { get; private set; }
+
+        public Card(string val, string suit)
+        {
+            String_Value = val;
+            Suit = suit;
+        }
+    }
+
+    public class CardEngine
+    {
+        public static Card CreateCard(string val, string suit)
+        {
+            return new Card(val, suit);
+        }
+    }
 }
